2bd94ef5f3414bbb574882340a16b42ec2181b47
Alex Ashery <alexander_ashery@brown.edu>
2016-02-21 18:10:16 -0500

    Interface for pulling show data.

---
+++ MALInterface.py
@@ -0,0 +1,22 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+class malInterface(object):
+
+    prefix_url = "http://myanimelist.net/api/anime|manga/search.xml?q="
+
+    def __init__(self, username, password):
+        self.auth = HTTPBasicAuth(username, password) #user authentication
+
+    def get_show_data(self, show):
+        #format show name
+        show = show.replace(" ", "+")
+        show = show.lower()
+        #request url
+        url = self.prefix_url + show
+        r = requests.get(url, auth=self.auth)
+        return r.text
+
+if __name__ == "__main__":
+    interface = malInterface('malInvestigation', 'griffithdidnothingwrong10')
+    print interface.get_show_data("Bleach")
